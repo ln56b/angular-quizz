d33548c6c59acdc78d17fdae79c10f79f4b85763
Hélène Baudrand <baudrand.helene@gmail.com>
2023-05-16 19:12:35 +0200

    refactor(added components folder): changed paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { QuizzFormComponent } from './quizz-form/quizz-form.component';
-import { QuizzCardComponent } from './quizz-card/quizz-card.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuizzFormComponent } from './components/quizz-form/quizz-form.component';
+import { QuizzCardComponent } from './components/quizz-card/quizz-card.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { QuizzListComponent } from './quizz-list/quizz-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const EXTERNAL = [BrowserModule, AppRoutingModule, HttpClientModule];
-const QUIZZ_APP = [QuizzListComponent];
+const EXTERNAL = [
+  BrowserModule,
+  AppRoutingModule,
+  HttpClientModule,
+  BrowserAnimationsModule,
+];
+const QUIZZ_APP = [DashboardComponent];
 
 @NgModule({
   declarations: [AppComponent],
