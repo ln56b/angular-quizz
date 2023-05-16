6af08c8bc98a12ce0cb5a67ddbc391df528c013b
Hélène Baudrand <baudrand.helene@gmail.com>
2023-05-16 18:42:09 +0200

    feat(added routing): app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { QuizzFormComponent } from './quizz-form/quizz-form.component';
+import { QuizzCardComponent } from './quizz-card/quizz-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'quizz/:id',
+    component: QuizzCardComponent,
+  },
+  {
+    path: 'quizz/edit/:id',
+    component: QuizzFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
