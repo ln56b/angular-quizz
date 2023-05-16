4ce019a9724735a5b16d97f49217f2d42c387ce0
Hélène Baudrand <baudrand.helene@gmail.com>
2023-05-16 08:49:44 +0200

    refactor(cleaned app ): from cli generated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '',
+  styleUrls: [],
 })
-export class AppComponent {
-  title = 'angular-quizz';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { QuizzListComponent } from './quizz-list/quizz-list.component';
+
+const EXTERNAL = [BrowserModule, AppRoutingModule, HttpClientModule];
+const QUIZZ_APP = [QuizzListComponent];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [EXTERNAL, QUIZZ_APP],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
