9bf63aebeff0d053c6c8b4adb6cff8f91cb1257f
Hélène Baudrand <baudrand.helene@gmail.com>
2023-05-22 23:02:27 +0200

    refactor(service): update catch errors

--- src/app/shared/services/quizz.service.ts
+++ src/app/shared/services/quizz.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { environment } from 'src/app/environments/environment';
 import { Quizz, QuizzNames } from '../models/quizz.models';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -15,16 +19,35 @@ export class QuizzService {
   loadQuizzes(): Observable<Quizz[]> {
     return this.http
       .get<Quizz[]>(this.ENDPOINT_URL)
-      .pipe(map((apiRes) => apiRes));
+      .pipe(catchError(this.handleError<Quizz[]>('loadQuizzes')));
   }
 
   startQuizz(name: QuizzNames): Observable<Quizz> {
     return this.http
-      .post<{ quizz: Quizz }>(this.ENDPOINT_URL, { name })
-      .pipe(map((apiRes) => apiRes.quizz));
+      .post<{ quizz: Quizz }>(this.ENDPOINT_URL, { name }, httpOptions)
+      .pipe(
+        map((apiRes) => apiRes.quizz),
+        catchError(this.handleError<Quizz>('startQuizz'))
+      );
   }
 
   getQuizz(id: number): Observable<Quizz> {
-    return this.http.get<Quizz>(`${this.ENDPOINT_URL}/${id}`);
+    return this.http
+      .get<Quizz>(`${this.ENDPOINT_URL}/${id}`)
+      .pipe(catchError(this.handleError<Quizz>('getQuizz')));
+  }
+
+  updateQuizz(quizz: Quizz): Observable<Quizz> {
+    const url = `${this.ENDPOINT_URL}/${quizz.id}`;
+    return this.http
+      .put<Quizz>(url, quizz, httpOptions)
+      .pipe(catchError(this.handleError<Quizz>('updateQuizz')));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    };
   }
 }
